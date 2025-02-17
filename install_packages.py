9821eab43277e2b46992049745dc7e97b2908f4f
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-02-17 13:06:20 -0300

    Add first complete implementation

--- install_packages.py
+++ install_packages.py
@@ -1,7 +1,7 @@
 """Read setup.cfg and run `pip` to install requirements (default and development)"""
+
 import subprocess
 from configparser import ConfigParser
-
 
 config = ConfigParser()
 config.read("setup.cfg")
@@ -9,5 +9,6 @@ config.read("setup.cfg")
 requirements = []
 requirements.extend(config.get("options", "install_requires", fallback="").strip().splitlines())
 requirements.extend(config.get("options.extras_require", "dev", fallback="").strip().splitlines())
+requirements.extend(config.get("options.extras_require", "img", fallback="").strip().splitlines())
 if requirements:
     subprocess.call(["pip", "install", "--no-cache-dir"] + requirements)
